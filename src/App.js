486c753323f0b54ea6780dd21313b759c10fcd75
Sanjay <ufoundsanjay@gmail.com>
2021-05-22 02:44:15 +0530

    Mock run with api

--- src/App.js
+++ src/App.js
@@ -2,14 +2,50 @@ import './App.css';
 import Header from './components/Header'
 import {Container, Grid} from "@material-ui/core";
 import ListingCard from './components/ListingCard'
+import {useEffect, useState} from "react";
 function App() {
+    const api = {
+        'image_url' : 'https://cdn.myanimelist.net/images/anime/13/17405.jpg',
+        'title_english': 'Naruto',
+        'episodes': 220,
+        'rating':'',
+        'status': 'Watching',
+        'airing': 'Yes'
+    }
+    const [loading,setLoading] = useState(true)
+    const [anime,setAnime] = useState({});
+    const [error,setError] = useState(null);
+    useEffect(
+        ()=> {
+            fetch('https://api.jikan.moe/v3/anime/20').then(
+                res => res.json()
+            ).then(
+                res => {
+                    setLoading(false);
+                    setAnime(res)
+                },
+                error => {
+                    setError(error);
+                }
+            )
+        }
+        ,[])
+    console.log(anime)
+    let val;
+    if(error){
+        val = <div>Error: {error.message}</div>
+    }else if(loading){
+        val = <div>Loading !!!!</div>
+    }else{
+        val = <ListingCard api={anime} />
+    }
   return (
     <div className="App">
         <Header />
         <Container className="mt-5" maxWidth="md" >
             <Grid container spacing={1}>
                 <Grid container item md={3}>
-                    <ListingCard />
+                    {val}
                 </Grid>
             </Grid>
         </Container>
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Grid, makeStyles} from "@material-ui/core";
 import Card from '@material-ui/core/Card';
-import {CardActions} from "@material-ui/core";  
+import {CardActions} from "@material-ui/core";
 import {CardContent} from "@material-ui/core";
 import {Button} from "@material-ui/core";
 import {Typography} from "@material-ui/core";
@@ -33,7 +33,7 @@ const useStyles = makeStyles({
         fontWeight:"bold"
     },
     logo:{
-        width: 30
+        width: 35
     },
     w100:{
         width:"100%"
@@ -43,7 +43,7 @@ const useStyles = makeStyles({
     }
 });
 
-function ListingCard(){
+function ListingCard(props){
     const classes = useStyles()
 
     return(
@@ -51,17 +51,17 @@ function ListingCard(){
             <CardContent>
                 {/* Grid */}
                 <Grid container spacing={1}>
-                    <Grid item xs={2}>
+                    <Grid item xs={3}>
 
                         {/* Anime Logo */}
-                        <img src="https://images.unsplash.com/photo-1621600052943-60cc6d3b4559?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" className={classes.logo} />
+                        <img src={props.api.image_url} className={classes.logo} />
 
                     </Grid>
 
                     {/* Anime Name */}
-                    <Grid item xs={10} className={classes.mAuto}>
+                    <Grid item xs={9} className={classes.mAuto}>
 
-                        <Typography variant="h6"  className={classes.value} >Naruto Uzumaki</Typography>
+                        <Typography variant="h6"  className={classes.value} >{props.api.title_english}</Typography>
 
                     </Grid>
 
@@ -69,7 +69,7 @@ function ListingCard(){
                         {/* Number of Episodes */}
 
                         <Typography variant="caption" className={classes.subHeading}>Episodes</Typography>
-                        <Typography  variant="h6" className={classes.value}> 20</Typography>
+                        <Typography  variant="h6" className={classes.value}> {props.api.episodes}</Typography>
 
                     </Grid>
 
@@ -78,7 +78,7 @@ function ListingCard(){
                         {/* Rating of Anime */}
 
                         <Typography variant="caption" className={classes.subHeading}>Rating</Typography>
-                        <Typography variant="h6" className={classes.value} > 9.40</Typography>
+                        <Typography variant="h6" className={classes.value} >{props.api.score}</Typography>
                     </Grid>
 
                     <Grid item xs={6}>
